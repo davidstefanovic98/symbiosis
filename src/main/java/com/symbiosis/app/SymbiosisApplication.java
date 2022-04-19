437a6c99618d3f5b4d643a6c3efcd3a7bd277c5b
davidstefanovic98 <david.stefanovic0711@gmail.com>
2022-04-19 22:13:53 +0200

    inital commit

---
+++ src/main/java/com/symbiosis/app/SymbiosisApplication.java
@@ -0,0 +1,13 @@
+package com.symbiosis.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymbiosisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymbiosisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/symbiosis/app/SymbiosisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symbiosis.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymbiosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
