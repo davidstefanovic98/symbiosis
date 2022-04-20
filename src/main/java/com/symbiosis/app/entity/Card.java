e1f02b3a4d931ff7afda600aa643948febca6a99
davidstefanovic98 <david.stefanovic0711@gmail.com>
2022-04-20 21:19:11 +0200

    added commit for testing

--- src/main/java/com/symbiosis/app/entity/Card.java
+++ src/main/java/com/symbiosis/app/entity/Card.java
@@ -30,6 +30,7 @@ public class Card extends Auditable{
     @ToString.Exclude
     private List<CardItem> cardItems;
 
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
